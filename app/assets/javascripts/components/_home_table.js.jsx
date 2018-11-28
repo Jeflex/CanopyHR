57157007a89ea38d1ed8e88b68c0f4ceac4216a8
pchassoun <pchassoun@miners.utep.edu>
2018-11-28 14:26:18 -0700

    sync

--- app/assets/javascripts/components/_home_table.js.jsx
+++ app/assets/javascripts/components/_home_table.js.jsx
@@ -17,37 +17,49 @@ class HomeTable extends React.Component {
   }
 
   render(){
-    return(
-      <table class="table" >
-        <thead class="thead thread-main">
+    if(this.state.employees.length == 0){
+      return(
+        <table class="table" >
+          <thead class="thead thread-main">
+            <tr>
+              <th scope="col" class="th-main">SSN</th>
+              <th scope="col" class="th-main">First Name</th>
+              <th scope="col" class="th-main">Last Name</th>
+              <th scope="col" class="th-main">Work Phone</th>
+            </tr>
+          </thead>
+        </table>
+      )
+    }
+    else{
+      var innerTable =[];
+
+      for(var i = 0; i < this.state.employees.length; i++){
+        innerTable.push(
           <tr>
-            <th scope="col" class="th-main">#</th>
-            <th scope="col" class="th-main">First</th>
-            <th scope="col" class="th-main">Last</th>
-            <th scope="col" class="th-main">Handle</th>
+            <th scope="row">{this.state.employees[i].ssn}</th>
+            <td class="td-main">{this.state.employees[i].firstname}</td>
+            <td class="td-main">{this.state.employees[i].lastname}</td>
+            <td class="td-main">{this.state.employees[i].workphone}</td>
           </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td class="td-main">Mark</td>
-            <td class="td-main">Otto</td>
-            <td class="td-main">@mdo</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td class="td-main">Jacob</td>
-            <td class="td-main">Thornton</td>
-            <td class="td-main">@fat</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td class="td-main">Larry</td>
-            <td class="td-main">the Bird</td>
-            <td class="td-main">@twitter</td>
-          </tr>
-        </tbody>
-      </table>
-     )
+        )
+      }
+
+      return(
+        <table class="table">
+          <thead class="thead thread-main">
+            <tr>
+              <th scope="col" class="th-main">SSN</th>
+              <th scope="col" class="th-main">First Name</th>
+              <th scope="col" class="th-main">Last Name</th>
+              <th scope="col" class="th-main">Work Phone</th>
+            </tr>
+          </thead>
+          <tbody>
+            {innerTable}
+          </tbody>
+        </table>
+      )
+    }
    }
 }
