caac4f3b0de170ee03c0b29efcf16b8a9caffe04
pchassoun <pchassoun@miners.utep.edu>
2018-11-28 04:28:18 -0700

    sync

--- app/assets/javascripts/components/_home_table.js.jsx
+++ app/assets/javascripts/components/_home_table.js.jsx
@@ -17,38 +17,37 @@ class HomeTable extends React.Component {
   }
 
   render(){
-    // <table class="table" >
-    //   <thead class="thead thread-main">
-    //     <tr>
-    //       <th scope="col" class="th-main">#</th>
-    //       <th scope="col" class="th-main">First</th>
-    //       <th scope="col" class="th-main">Last</th>
-    //       <th scope="col" class="th-main">Handle</th>
-    //     </tr>
-    //   </thead>
-    //   <tbody>
-    //     <tr>
-    //       <th scope="row">1</th>
-    //       <td class="td-main">Mark</td>
-    //       <td class="td-main">Otto</td>
-    //       <td class="td-main">@mdo</td>
-    //     </tr>
-    //     <tr>
-    //       <th scope="row">2</th>
-    //       <td class="td-main">Jacob</td>
-    //       <td class="td-main">Thornton</td>
-    //       <td class="td-main">@fat</td>
-    //     </tr>
-    //     <tr>
-    //       <th scope="row">3</th>
-    //       <td class="td-main">Larry</td>
-    //       <td class="td-main">the Bird</td>
-    //       <td class="td-main">@twitter</td>
-    //     </tr>
-    //   </tbody>
-    // </table>
     return(
-
+      <table class="table" >
+        <thead class="thead thread-main">
+          <tr>
+            <th scope="col" class="th-main">#</th>
+            <th scope="col" class="th-main">First</th>
+            <th scope="col" class="th-main">Last</th>
+            <th scope="col" class="th-main">Handle</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td class="td-main">Mark</td>
+            <td class="td-main">Otto</td>
+            <td class="td-main">@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td class="td-main">Jacob</td>
+            <td class="td-main">Thornton</td>
+            <td class="td-main">@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td class="td-main">Larry</td>
+            <td class="td-main">the Bird</td>
+            <td class="td-main">@twitter</td>
+          </tr>
+        </tbody>
+      </table>
      )
    }
 }
